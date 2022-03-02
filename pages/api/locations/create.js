46984c22d0260a8ca9b0bbfba1b98767572dc557
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 10:52:15 -0500

    Implement try block of api/locations/create.

--- pages/api/locations/create.js
+++ pages/api/locations/create.js
@@ -1,17 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from "@prisma/client";
 
 export default async function(req, res){
   const prisma = new PrismaClient({ log: ["query"] });
 
   try {
-
+      const {location: locationData} = req.body;
+      const location = await prisma.locations.create({
+        data: {
+          latitude:locationData.latitude,
+          longitude: locationData.longitude,
+        },
+      });
+      res.status(201);
+      res.json({location});
   } catch (err) {
+    console.log(err);
     res.status(500);
     res.json({error: "Unable to handle request"});
   } finally {
     await prisma.$disconnect();
   }
-
-  res.status(200);
-  res.json({ location: "created"});
 }
