42c07f062e9c04f4e07dd79166251824d3c3fd0b
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 10:07:37 -0500

    Remove template API. Add prismaClient connection & try/catch block w/ error response to all API routes.

--- pages/api/users/create.js
+++ pages/api/users/create.js
@@ -1,5 +1,19 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient, prismaClient } from "@prisma/client";
 
-export default async function(NextApiRequest, NextApiResponse){
+export default async function(req, res){
   
+  const prisma = new PrismaClient({ log: ["query"] });
+
+  try {
+
+  } catch (err) {
+    res.status(500);
+    res.json({error: "Unable to handle request"});
+  } finally {
+    await prisma.disconnect();
+  }
+
+  res.status(200);
+  res.json({ user: "created"});
 }
