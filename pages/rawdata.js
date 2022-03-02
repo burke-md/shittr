4cac2430ed47b26b3e3edd6454048da0408b84ff
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 08:08:09 -0500

    Add pages w/ simple boilerplate.

---
+++ pages/rawdata.js
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import Head from 'next/head'
+
+export default function RawData(){
+  return (
+    <>
+      <Head>
+        <title></title>
+      </Head>
+      <h1></h1>
+      <h2>
+        <Link href="/">
+          <a>home</a>
+        </Link>
+      </h2>
+    </>
+  )
+}
