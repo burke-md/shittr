01f962ad5aca58248e1cfff9e7381fb64713af10
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 10:34:34 -0500

    Implement try block of api/users/create.

--- pages/api/users/create.js
+++ pages/api/users/create.js
@@ -1,19 +1,26 @@
 import { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient, prismaClient } from "@prisma/client";
+import { PrismaClient } from "@prisma/client";
 
 export default async function(req, res){
   
   const prisma = new PrismaClient({ log: ["query"] });
 
   try {
+    const {user: userData} = req.body;
+    const user = await prisma.users.create({
+      data: {
+        userName:userData.userName,
+        passHash: userData.passHash,
+      },
+    });
 
+    res.status(201);
+    res.json({user});
   } catch (err) {
+    console.log(err);
     res.status(500);
     res.json({error: "Unable to handle request"});
   } finally {
     await prisma.$disconnect();
   }
-
-  res.status(200);
-  res.json({ user: "created"});
 }
