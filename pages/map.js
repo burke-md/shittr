67415b747da74d6295ad851977dd33e9d732edb9
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 15:40:33 -0500

    Implement bare bones map page.

--- pages/map.js
+++ pages/map.js
@@ -1,18 +1,43 @@
-import Link from 'next/link'
-import Head from 'next/head'
+import React from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
 
-export default function Map(){
+const libraries = ["places"];
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+const options = {
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 50,
+  lng: -50,
+};
+
+export default function App() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading map...";
+
   return (
-    <>
-      <Head>
-        <title></title>
-      </Head>
-      <h1></h1>
-      <h2>
-        <Link href="/">
-          <a>home</a>
-        </Link>
-      </h2>
-    </>
-  )
+    <GoogleMap
+      id="map"
+      mapContainerStyle={mapContainerStyle}
+      zoom={8}
+      center={center}
+      options={options}
+      onLoad={onMapLoad}
+    >
+    </GoogleMap>
+  );
 }
