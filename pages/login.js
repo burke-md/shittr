e381d05bc6c813be60cc46323452ed719c58a942
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 13:27:51 -0500

    Create and import empty login component.

--- pages/login.js
+++ pages/login.js
@@ -1,18 +1,23 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import LoginForm from '../components/LoginForm'
 
 export default function Login(){
   return (
     <>
       <Head>
-        <title></title>
+        <title>Shittr-Login</title>
       </Head>
+      <main>
       <h1></h1>
+      <LoginForm />
       <h2>
         <Link href="/">
           <a>home</a>
         </Link>
       </h2>
+      </main>
+      
     </>
   )
 }
