d89fea982a4a0110c7d75bd97ab15a11aa46c895
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 14:04:16 -0500

    Build form component for login and connect to api route.No style.

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -1,6 +1,27 @@
 export default function LoginForm() {
-  return (
-      <>
-      </>
+  const loginUser = async event => {
+    event.preventDefault()
+
+    const res = await fetch('/api/users/login', {
+      body: JSON.stringify({
+        username: event.target.username.value,
+        password: event.target.password.value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      method: 'POST'
+    })
+
+    const result = await res.json();
+  }
+  return (   
+      <form >
+        <label htmlFor="username">username</label>
+        <input id="username" name="username" type="text" autoComplete="username" required />
+        <label htmlFor="password">password</label>
+        <input id="password" name="password" type="text" autoComplete="name" required />
+        <button type="submit">Log in</button>
+    </form>    
   )
 }
