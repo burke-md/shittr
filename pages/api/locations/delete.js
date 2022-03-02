3539aa1ca517c874499bb89e3f69b51c0f375cb2
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 11:19:06 -0500

    Implement try block of api/locations/delete.

--- pages/api/locations/delete.js
+++ pages/api/locations/delete.js
@@ -1,17 +1,22 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
 
 export default async function(req, res){
   const prisma = new PrismaClient({ log: ["query"] });
 
   try {
-
+    const {location: locationData} = req.body;
+    const deleteLocation = await prisma.locations.delete({
+      where: {
+        id: locationData.id,
+      },
+    })
+    res.status(200);
+    res.json({ deleteLocation});
   } catch (err) {
     res.status(500);
     res.json({error: "Unable to handle request"});
   } finally {
     await prisma.$disconnect();
   }
-
-  res.status(200);
-  res.json({ location: "deleted"});
 }
