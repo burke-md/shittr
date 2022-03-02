c33d63621b8d9d65cbc09eae2bbdc1340cde5921
Michael Burke <michael.dburke56@gmail.com>
2022-03-02 11:12:20 -0500

    Implement try block of api/locations/update.

--- pages/api/locations/update.js
+++ pages/api/locations/update.js
@@ -1,17 +1,31 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { PrismaClient } from '@prisma/client';
 
 export default async function(req, res){
   const prisma = new PrismaClient({ log: ["query"] });
 
   try {
+    const {location: locationData} = req.body;
 
+    console.log(`\n locationData\n ${locationData} req.body: ${JSON.stringify(req.body)}`);
+
+    const updateLocation = await prisma.locations.update({
+      where: {
+        id: locationData.id,
+      },
+      data: {
+        latitude: locationData.latitude,
+        longitude: locationData.longitude,
+      },
+    })
+
+    res.status(200);
+    res.json({updateLocation});
   } catch (err) {
+    console.log(err);
     res.status(500);
     res.json({error: "Unable to handle request"});
   } finally {
     await prisma.$disconnect();
   }
-
-  res.status(200);
-  res.json({ location: "updated"});
 }
