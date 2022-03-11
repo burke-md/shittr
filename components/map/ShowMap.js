e8fe6d3379b8667813fe6213b4b90e4c305b1ca2
Michael Burke <michael.dburke56@gmail.com>
2022-03-10 19:37:14 -0500

    Break out map visual component from map page.

---
+++ components/map/ShowMap.js
@@ -0,0 +1,134 @@
+import React from "react";
+import { useCallback } from "react";
+import { QueryClientProvider, QueryClient, useQuery, useMutation } from "react-query";
+import { GoogleMap, useLoadScript, Marker, Data } from "@react-google-maps/api";
+import mapStyleOverride from "../../data/mapStyleOverride";
+
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  height: "100vh",
+  width: "100vw",
+};
+const options = {
+  styles: mapStyleOverride,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 43.6532,
+  lng: -79.3832,
+};
+
+const queryClient = new QueryClient()
+
+async function fetchLocationsReq(){
+  const res = await fetch("/api/locations/qq", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({read: {"locations": ""}})
+  });
+  const data = await res.json();
+  return data.crudOpsReturn[0];
+}
+
+async function createLocationReq(locationData) {
+  console.log(`called create`)
+  const res = await fetch("/api/locations/qq", {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json"
+    },
+    body: JSON.stringify({ create: {
+        ...locationData
+      }
+    })
+  })
+  const data = await res.json();
+  console.log(`data: ${data}`)
+  console.log(`data: ${JSON.stringify(data)}`)
+  const { location } = data;
+  return location;
+}
+
+
+export default function ShowMap() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    libraries,
+  });
+
+  const {data: locationsData, status} = useQuery("locationsData", fetchLocationsReq);
+
+  const mutation = useMutation(createLocationReq, {
+    /*Optimistic update
+    -> Stop queries, preserve existing data, force update data with temp key
+    -> If error, roll back to prev state
+    -> Refetch & confirm. Update key w/ real key from db
+    */
+    
+    onMutate: (newData) => {
+      queryClient.cancelQueries("locationsData");
+      const existing = queryClient.getQueryData("locationsData");
+      queryClient.setQueryData("locationsData", (prev) => [
+        ...prev,
+        {...newData, id: new Date().toISOString() },
+      ]);
+      //return cache state for rollback
+      return existing;
+    },
+    onError: (error, newData, rollback) => rollback(),
+    onSettled: () => queryClient.refetchQueries("locations"),
+  });
+
+  const onMapClick = useCallback((e) => {
+    console.log(`onmapclick`)
+    mutation.mutate({
+      latitude: e.latLng.lat(),
+      longitude: e.latLng.lng()
+    })
+    console.log(`onmapclick after mutation call`)
+  }, []);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading map...";
+
+  return (
+    <>
+    {status === "loading" && <p>Loading..</p>}
+
+    {status === "success"  &&(
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+        >
+      {locationsData?.map((location) => (
+        <Marker 
+        key={location.id}
+        position={{
+          lat: Number(location.latitude), 
+          lng: Number(location.longitude)
+        }}
+        />
+      ))}  
+      </GoogleMap>
+    )}
+    </>
+  );
+}
+
+
+
+
